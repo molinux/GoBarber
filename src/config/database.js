319a94da717a592009d59b28b4e43c8f0752b931
Molinux <cybercrowbr@gmail.com>
2020-04-25 18:00:00 -0300

    update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,6 @@
 module.exports = {
   dialect: 'postgres',
-<<<<<<< HEAD
   host: '192.168.0.11',
-=======
-  host: 'localhost',
->>>>>>> ea6125450c6562f65eb46c6ed2685b7a736e7a17
   username: 'postgres',
   password: 'docker',
   database: 'gobarber',
