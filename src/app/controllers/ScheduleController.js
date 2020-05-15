386ce87a749c636dd32f500bea6f2c42aa956178
Molinux <cybercrowbr@gmail.com>
2020-05-15 12:13:35 -0300

    Backend scheduling and agenda

---
+++ src/app/controllers/ScheduleController.js
@@ -0,0 +1,39 @@
+import { startOfDay, endOfDay, parseISO } from 'date-fns';
+import { Op } from 'sequelize';
+
+import User from '../models/User';
+import Appointment from '../models/Appointment';
+
+class ScheduleController {
+  async index(req, res) {
+    const checkUserProvider = await User.findOne({
+      where: { id: req.userId, provider: true },
+    });
+
+    if (!checkUserProvider) {
+      return res.status(401).json({ error: 'User is not a provider' });
+    }
+
+    // Para o prestador, o importante é ver os agendamentos do dia
+    const { date } = req.query;
+    const parsedDate = parseISO(date);
+
+    const appointments = await Appointment.findAll({
+      where: {
+        // Todos os agendamentos do usuario logado
+        provider_id: req.userId,
+        canceled_at: null,
+        date: {
+          [Op.between]: [startOfDay(parsedDate), endOfDay(parsedDate)],
+        },
+      },
+      // Organizar os agendamentos por data
+      order: ['date'],
+    });
+
+    // Listar o resultado da query
+    return res.json(appointments);
+  }
+}
+
+export default new ScheduleController();
